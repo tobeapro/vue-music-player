ac67e4d5a7ccb75e1033472737e13fec077a7ff4
rk <471091127@qq.com>
2017-09-13 23:11:54 +0800

    v1

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 .tab{
   display:flex;
