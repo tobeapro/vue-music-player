5de4b25c4791af5ce27e252dc604c678f9c4a3d8
rk <471091127@qq.com>
2017-12-04 17:30:24 +0800

    songlist

--- src/components/songList.vue
+++ src/components/songList.vue
@@ -2,17 +2,18 @@
   transition(name="song-fade")
     div(class="song-list",v-if="songListStatus")
       div(class="song-list-header-mask",:style="{background:'url('+songList.bgImg+')',backgroundSize:'cover'}")
-      div(class="song-list-header")
-        div(class="song-list-title")
-          i(class="fa fa-arrow-left fa-x",@click="hideSongList")
-          span 歌单
-        div(class="song-list-info")
-          div(class="song-list-pic")
-            img(:src="songList.bgImg")
-      div(class="song-list-content")
-        div(class="song-list-item",v-for="(item,index) in songList.list")
-          span(class="song-list-item-index") {{index+1}}、
-          span(class="song-list-item-name") {{item.name}}
+      div(class="song-list-title")
+        i(class="fa fa-arrow-left fa-2x",@click="hideSongList")
+        span 歌单
+      div(class="song-list-part")
+        div(class="song-list-header")
+          div(class="song-list-info")
+            div(class="song-list-pic")
+              img(:src="songList.bgImg")
+        div(class="song-list-content")
+          div(class="song-list-item",v-for="(item,index) in songList.list")
+            span(class="song-list-item-index") {{index+1}}、
+            span(class="song-list-item-name") {{item.name}}
 </template>
 
 <script>
@@ -45,50 +46,67 @@ export default{
     left:0;
     right:0;
     bottom:0;
-    z-index:9;
+    z-index:8;
     background:#fff;
+    &.song-fade,
     .song-list-header-mask{
-      height:200px;
-      overflow:hidden;
-      filter:blur(30px);
-    }
-    .song-list-header{
       position:absolute;
+      z-index:-1;
       top:0;
       left:0;
       right:0;
       height:200px;
-      font-size:12px;
+      overflow:hidden;
+      filter:blur(30px);
+    }
+    .song-list-title {
+      position: fixed;
+      z-index:1;
+      left: 0;
+      right: 0;
+      top: 0;
+      height: 40px;
+      line-height: 40px;
+      font-size: 16px;
       color:#fff;
-      .song-list-title{
-        height:40px;
-        line-height:40px;
-        font-size:16px;
-        .fa{
-          margin:0 20px;
-        }
+      .fa {
+        font-size: 16px;
+        margin: 0 10px;
+        padding: 6px;
       }
-      .song-list-info{
-        .song-list-pic{
-          img{
-            width:80px;
-            height:80px;
+    }
+    .song-list-part {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 50px;
+      overflow-y:auto;
+      .song-list-header {
+        padding-top: 40px;
+        height: 200px;
+        font-size: 12px;
+        color: #fff;
+        overflow-y: auto;
+        background: rgba(0, 0, 0, .6);
+        .song-list-info {
+          .song-list-pic {
+            img {
+              width: 80px;
+              height: 80px;
+            }
           }
         }
       }
-    }
-    .song-list-content{
-      position:fixed;
-      top:200px;
-      left:0;
-      right:0;
-      bottom:0;
-      background:#fff;
-      .song-list-item{
-        padding-left:12px;
-        height:36px;
-        line-height:36px;
-        font-size:12px;
+      .song-list-content {
+        background: #fff;
+        .song-list-item {
+          padding-left: 12px;
+          height: 36px;
+          line-height: 36px;
+          font-size: 12px;
+          border-bottom:1px solid #ddd;
+        }
       }
     }
   }
